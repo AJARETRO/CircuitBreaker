4154756b77c8d0e86101872e7e89cecefcef8d9d
AJA RETRO <niloyahmed2484@gmail.com>
2025-11-12 01:23:25 +0600

    Initial commit

---
+++ src/main/java/dev/ajaretro/circuitBreaker/CircuitBreaker.java
@@ -0,0 +1,17 @@
+package dev.ajaretro.circuitBreaker;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CircuitBreaker extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
---
+++ src/main/java/dev/ajaretro/circuitBreaker/LagListener.java
@@ -0,0 +1,4 @@
+package dev.ajaretro.circuitBreaker;
+
+public class LagListener {
+}
